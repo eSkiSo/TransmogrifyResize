fb524077f5138d2638320a0348cec498751a1d09
Cosmic Cleric <cosmiccleric@gmail.com>
2025-07-10 16:51:55 -0700

    * Added saving of window size between opening of window, so that it doesn't have to be resized each time. * Added additional info to the TOC file for Internationalization, etc.

--- TransmogrifyResize.lua
+++ TransmogrifyResize.lua
@@ -1,75 +1,97 @@
 ﻿
 local f, events = CreateFrame("Frame"), {};
-f:RegisterEvent("TRANSMOG_COLLECTION_UPDATED")
---f:RegisterAllEvents();
 
-f:SetScript("OnEvent", function(self,event, ...)
-    if event == "TRANSMOG_COLLECTION_UPDATED" then
-        -- print("Improving Transmog window...")
-        ResizeTransmogrify();
-    end  
+-- Use TRANSMOG_COLLECTION_UPDATED event to be sure WardrobeFrame is not nil, as can still be nil on TRANSMOGRIFY_OPEN!
+f:RegisterEvent("TRANSMOG_COLLECTION_UPDATED") -- *NOT 'TRANSMOGRIFY_OPEN'!*  https://warcraft.wiki.gg/wiki/Events#Transmogrify
+
+f:SetScript("OnEvent", function(self, event, ...)
+    -- No need to check for which event before calling resize function;
+    -- just only listening for the one we want.
+    TransmogResize_ResizeTransmogrify(TransmogResize_FrameWidth, TransmogResize_FrameHeight)
 end)
+
+SLASH_TRANSMOGRESIZE1 = "/tmr"
+SLASH_TRANSMOGRESIZE2 = "/transmogresize"
+SLASH_TRANSMOGRESIZE3 = "/transmogrifyresize"
+SLASH_TRANSMOGRESIZE4 = "/transmogrifierresize"
+function SlashCmdList.TRANSMOGRESIZE(msg)
+    if ( (msg == "help") or (not msg) or (msg:gsub("%s+", "") == "") ) then
+        print("\n")
+        print("Transmogrify Resize")
+        print("  Type '/tmr reset' to reset the size of the Transmogrifier window.")
+        print("  Type '/tmr help' (or just '/tmr') for this help information.")
+        print("\n")
+    elseif msg == "reset" then
+        TransmogResize_FrameWidth = nil
+        TransmogResize_FrameHeight = nil
+        TransmogResize_ResizeTransmogrify(TransmogResize_FrameWidth, TransmogResize_FrameHeight)
+        print("Transmogrify Resize - Transmogrifier's window size has been reset!")
+    end
+end
 
 local isAddedResize = false
 
-function ResizeTransmogrify(frameWidth, frameHeight)
-    if frameWidth == nil then
+function TransmogResize_ResizeTransmogrify(frameWidth, frameHeight)
+
+    -- Doing this check as event TRANSMOG_COLLECTION_UPDATED gets called
+    -- when char loads into World, before being at transmog npc window.
+    if not C_Transmog.IsAtTransmogNPC() then return end
+
+    if not frameWidth then
         frameWidth = GetScreenWidth() - 460;   -- 1462
     end
-    
-    if frameHeight == nil then
+    TransmogResize_FrameWidth = frameWidth
+
+    if not frameHeight then
         frameHeight = GetScreenHeight() - 240; -- 841
     end
-    local modelSceneWidth = frameWidth - 662; -- 800
-    local modelSceneHeight = frameHeight - 111; -- 730
-    if WardrobeFrame ~= nil then
-        WardrobeFrame:SetWidth(frameWidth);
-        WardrobeFrame:SetHeight(frameHeight);
-        WardrobeTransmogFrame.ModelScene:SetWidth(modelSceneWidth);
-        WardrobeTransmogFrame:SetWidth(modelSceneWidth);
-        WardrobeTransmogFrame:SetHeight(modelSceneHeight);
-        WardrobeTransmogFrame.ModelScene:SetHeight(modelSceneHeight);
-        WardrobeTransmogFrame.Inset.BG:SetWidth(modelSceneWidth);
-        WardrobeTransmogFrame.Inset.BG:SetHeight(modelSceneHeight);
-        WardrobeTransmogFrame.HeadButton:SetPoint("LEFT", WardrobeTransmogFrame.ModelScene,"LEFT", 15, 100);
-        WardrobeTransmogFrame.HandsButton:SetPoint("TOPRIGHT", WardrobeTransmogFrame.ModelScene,"TOPRIGHT", -15, 0);
-        WardrobeTransmogFrame.MainHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", -50, 15);
-        WardrobeTransmogFrame.SecondaryHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", 50, 15);
-        WardrobeTransmogFrame.MainHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", 100, 20);
-        WardrobeTransmogFrame.SecondaryHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", 100, 20);
-        WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox:SetPoint("LEFT", WardrobeTransmogFrame.ModelScene,"TOPRIGHT", 50, -200);
-        WardrobeFrame:ClearAllPoints();
-        WardrobeFrame:SetPoint("CENTER", UIParent ,"CENTER",0,0);
+    TransmogResize_FrameHeight = frameHeight
 
-        if isAddedResize == false then
-            local resizeButton = CreateFrame("Button", nil, WardrobeFrame)
-            resizeButton:SetSize(16, 16)
-            resizeButton:SetPoint("BOTTOMRIGHT")
-            resizeButton:SetNormalTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Up")
-            resizeButton:SetHighlightTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Highlight")
-            resizeButton:SetPushedTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Down")
-             
-            resizeButton:SetScript("OnMouseDown", function(self, button)
-                WardrobeFrame:SetResizable(true)
-                WardrobeFrame:StartSizing("BOTTOMRIGHT")
-                WardrobeFrame:SetUserPlaced(true)
+    local modelSceneWidth = TransmogResize_FrameWidth - 662; -- 800
+    local modelSceneHeight = TransmogResize_FrameHeight - 111; -- 730
 
-            end)
-             
-            resizeButton:SetScript("OnMouseUp", function(self, button)
-                WardrobeFrame:StopMovingOrSizing()
-                local newWidth = WardrobeFrame:GetWidth()
-                local newHeight = WardrobeFrame:GetHeight()
-                ResizeTransmogrify(newWidth, newHeight);
+    WardrobeFrame:SetWidth(TransmogResize_FrameWidth);
+    WardrobeFrame:SetHeight(TransmogResize_FrameHeight);
+    WardrobeTransmogFrame.ModelScene:SetWidth(modelSceneWidth);
+    WardrobeTransmogFrame:SetWidth(modelSceneWidth);
+    WardrobeTransmogFrame:SetHeight(modelSceneHeight);
+    WardrobeTransmogFrame.ModelScene:SetHeight(modelSceneHeight);
+    WardrobeTransmogFrame.Inset.BG:SetWidth(modelSceneWidth);
+    WardrobeTransmogFrame.Inset.BG:SetHeight(modelSceneHeight);
+    WardrobeTransmogFrame.HeadButton:SetPoint("LEFT", WardrobeTransmogFrame.ModelScene,"LEFT", 15, 100);
+    WardrobeTransmogFrame.HandsButton:SetPoint("TOPRIGHT", WardrobeTransmogFrame.ModelScene,"TOPRIGHT", -15, 0);
+    WardrobeTransmogFrame.MainHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", -50, 15);
+    WardrobeTransmogFrame.SecondaryHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", 50, 15);
+    WardrobeTransmogFrame.MainHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", 100, 20);
+    WardrobeTransmogFrame.SecondaryHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene,"BOTTOM", 100, 20);
+    WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox:SetPoint("LEFT", WardrobeTransmogFrame.ModelScene,"TOPRIGHT", 50, -200);
+    WardrobeFrame:ClearAllPoints();
+    WardrobeFrame:SetPoint("CENTER", UIParent ,"CENTER",0,0);
 
-            end)
-            isAddedResize = true
-        end
+    if not isAddedResize then
+        local resizeButton = CreateFrame("Button", nil, WardrobeFrame)
+        resizeButton:SetSize(16, 16)
+        resizeButton:SetPoint("BOTTOMRIGHT")
+        resizeButton:SetNormalTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Up")
+        resizeButton:SetHighlightTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Highlight")
+        resizeButton:SetPushedTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Down")
 
-    else
-        print("TransmogrifyResize: Event occured but no Wardrobe found.")
+        resizeButton:SetScript("OnMouseDown", function(self, button)
+        WardrobeFrame:SetResizable(true)
+        WardrobeFrame:StartSizing("BOTTOMRIGHT")
+        WardrobeFrame:SetUserPlaced(true)
+
+        end)
+
+        resizeButton:SetScript("OnMouseUp", function(self, button)
+        WardrobeFrame:StopMovingOrSizing()
+        local newWidth = WardrobeFrame:GetWidth()
+        local newHeight = WardrobeFrame:GetHeight()
+        TransmogResize_ResizeTransmogrify(newWidth, newHeight);
+        end)
+
+        isAddedResize = true
     end
+
     -- WardrobeFrame:SetUserPlaced(true);
 end
-
-
